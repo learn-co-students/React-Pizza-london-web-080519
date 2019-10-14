8ca69f6352135c34bcc7266b1925c7877703a6f8
PollyCR <polly.c.reynolds@gmail.com>
2019-10-14 13:44:13 +0100

    Done.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,82 @@
-import React, { Component, Fragment } from 'react';
-import Header from './components/Header'
-import PizzaForm from './components/PizzaForm'
-import PizzaList from './containers/PizzaList'
+import React, { Component, Fragment } from "react";
+import Header from "./components/Header";
+import PizzaForm from "./components/PizzaForm";
+import PizzaList from "./containers/PizzaList";
 class App extends Component {
+  state = {
+    pizzas: [],
+    pizzaToEdit: []
+  };
+
+  componentDidMount = () => {
+    this.getPizzas();
+  };
+
+  getPizzas = () => {
+    fetch("http://localhost:3000/pizzas")
+      .then(resp => resp.json())
+      .then(pizzas => this.setState({ pizzas: pizzas }));
+  };
+
+  editPizza = pizza => {
+    this.setState({
+      pizzaToEdit: {
+        id: pizza.id,
+        topping: pizza.topping,
+        size: pizza.size,
+        vegetarian: pizza.vegetarian
+      }
+    });
+  };
+
+  editTopping = newValue => {
+    this.setState({
+      pizzaToEdit: { ...this.state.pizzaToEdit, topping: newValue }
+    });
+  };
+  editSize = newValue => {
+    this.setState({
+      pizzaToEdit: { ...this.state.pizzaToEdit, size: newValue }
+    });
+  };
+
+  editVeggie = () => {
+    this.setState({
+      pizzaToEdit: {
+        ...this.state.pizzaToEdit,
+        vegetarian: !this.state.pizzaToEdit.vegetarian
+      }
+    });
+  };
+
+  updatePizza = () => {
+    fetch(`http://localhost:3000/pizzas/${this.state.pizzaToEdit.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        topping: this.state.pizzaToEdit.topping,
+        size: this.state.pizzaToEdit.size,
+        vegetarian: this.state.pizzaToEdit.vegetarian
+      })
+    }).then(this.getPizzas);
+  };
+
   render() {
     return (
       <Fragment>
-        <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        {/* {console.log(this.state.pizzas)} */}
+        <Header />
+        <PizzaForm
+          pizzaToEdit={this.state.pizzaToEdit}
+          editTopping={this.editTopping}
+          editSize={this.editSize}
+          editVeggie={this.editVeggie}
+          updatePizza={this.updatePizza}
+        />
+
+        <PizzaList pizzas={this.state.pizzas} editPizza={this.editPizza} />
       </Fragment>
     );
   }
--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,14 +1,16 @@
+
 import React from "react"
 
-const Pizza = () => {
+class Pizza extends React.Component{
+  render(){
   return(
     <tr>
-      <td>{"Replace Me With Pizza Topping"}</td>
-      <td>{"Replace Me With Pizza Size"}</td>
-      <td>{"Replace Me With Vegatarian"}</td>
-      <td><button type="button" className="btn btn-primary">Edit Pizza</button></td>
+      <td>{this.props.pizza.topping}</td>
+      <td>{this.props.pizza.size}</td>
+      <td>{this.props.pizza.vegetarian ? "Yes" : "No"}</td>
+      <td><button type="button" className="btn btn-primary" onClick={() => this.props.editPizza(this.props.pizza)}>Edit Pizza</button></td>
     </tr>
   )
 }
-
+}
 export default Pizza
